184da3aad0e56bd0e998b5a21eb369c630d38b37
Katy Chadwell <kchadwell0226@gmail.com>
2021-09-22 21:33:52 -0500

    added a create profile button to users who do not already have one

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
@@ -15,7 +16,10 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
             <i className="fas fa-user"></i>{' '}
             Welcome, {user && user.name}!
         </p>
-        {profile !== null ? <Fragment>has</Fragment> : <Fragment>has not</Fragment>}
+        {profile !== null ? <Fragment>has</Fragment> : <Fragment>
+            <p>You have not yet set up a profile, please add some info.</p>
+            <Link to="/create-profile" className="btn btn-primary my-1">Create Profile</Link>
+        </Fragment>}
     </Fragment>;
 };
 
