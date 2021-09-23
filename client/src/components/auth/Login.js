fe81c7c9170036368666a04bc4ba9d0ea60686de
Katy Chadwell <kchadwell0226@gmail.com>
2021-09-22 19:24:21 -0500

    added login alerts

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../../actions/auth';
 
-const Login = () => {
+const Login = ({ login }) => {
     const [formData, setFormData] = useState({
         email: '',
         password: ''
@@ -12,7 +15,7 @@ const Login = () => {
     const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
     const onSubmit = async e => {
         e.preventDefault();
-        console.log('SUCESS');
+        login(email, password);
     };
 
     return (
@@ -47,6 +50,10 @@ const Login = () => {
             </p>
         </Fragment>
     )
-}
+};
 
-export default Login;
+Login.propTypes = {
+    login: PropTypes.func.isRequired
+};
+
+export default connect(null, { login })(Login);
