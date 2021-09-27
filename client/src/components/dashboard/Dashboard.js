6b861a76a6f720aed24e7176b3f0c567aa89ada3
Katy Chadwell <kchadwell0226@gmail.com>
2021-09-27 16:29:48 -0500

    added dashboard edit, experience, and education buttons

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import DashboardActions from './DashboardActions';
 import { getCurrentProfile } from '../../actions/profile';
 
 const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
@@ -10,17 +11,27 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
         getCurrentProfile();
     }, [getCurrentProfile]);
 
-    return loading && profile === null ? <Spinner /> : <Fragment>
-        <h1 className="large text-primary">Dashboard</h1>
-        <p className="lead">
-            <i className="fas fa-user"></i>{' '}
-            Welcome, {user && user.name}!
-        </p>
-        {profile !== null ? <Fragment>has</Fragment> : <Fragment>
-            <p>You have not yet set up a profile, please add some info.</p>
-            <Link to="/create-profile" className="btn btn-primary my-1">Create Profile</Link>
-        </Fragment>}
-    </Fragment>;
+    return loading && profile === null ? (
+        <Spinner />
+    ) : (
+        <Fragment>
+            <h1 className="large text-primary">Dashboard</h1>
+            <p className="lead">
+                <i className="fas fa-user"></i>{' '}
+                Welcome, {user && user.name}!
+            </p>
+            {profile !== null ? (
+                <Fragment>
+                    <DashboardActions />
+                </Fragment>
+            ) : (
+                <Fragment>
+                    <p>You have not yet set up a profile, please add some info.</p>
+                    <Link to="/create-profile" className="btn btn-primary my-1">Create Profile</Link>
+                </Fragment>
+            )}
+        </Fragment>
+    );
 };
 
 Dashboard.propTypes = {
