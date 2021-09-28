2dd0ef87ad9b488b92e8211839d6b90d77e8fa4e
Katy Chadwell <kchadwell0226@gmail.com>
2021-09-28 14:12:12 -0500

    added social media links and profile data to the profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getProfileById } from '../../actions/profile';
+import ProfileTop from './ProfileTop';
 
 const Profile = ({
     getProfileById,
@@ -13,12 +14,15 @@ const Profile = ({
 }) => {
     useEffect(() => {
         getProfileById(match.params.id);
-    }, [getProfileById]);
+    }, [getProfileById, match.params.id]);
 
     return <Fragment>
         {profile === null || loading ? <Spinner /> : <Fragment>
             <Link to="/profiles" className="btn btn-light">Back to Profiles</Link>
             {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (<Link to="/edit-profile" className="btn btn-dark">Edit Profile</Link>)}
+            <div class="profile-grid my-1">
+                <ProfileTop profile={profile} />
+            </div>
         </Fragment>}
     </Fragment>
 };
