ef4a17899421180e009a69530cb6d48454679e4f
Katy Chadwell <kchadwell0226@gmail.com>
2021-09-28 14:52:13 -0500

    added github repos to profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -8,6 +8,7 @@ import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
 import ProfileExperience from './ProfileExperience';
 import ProfileEducation from './ProfileEducation';
+import ProfileGithub from './ProfileGithub';
 
 const Profile = ({
     getProfileById,
@@ -42,6 +43,9 @@ const Profile = ({
                         ))}
                     </Fragment>) : (<h4>No Education Credentials</h4>)}
                 </div>
+                {profile.githubusername && (
+                    <ProfileGithub username={profile.githubusername} />
+                )}
             </div>
         </Fragment>
         }
