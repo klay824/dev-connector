ef5f8cf0018146fbd5cc084b7ef32f5cef9f0537
Katy Chadwell <kchadwell0226@gmail.com>
2021-09-28 14:21:34 -0500

    added bio and skills to profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getProfileById } from '../../actions/profile';
 import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
 
 const Profile = ({
     getProfileById,
@@ -22,6 +23,7 @@ const Profile = ({
             {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (<Link to="/edit-profile" className="btn btn-dark">Edit Profile</Link>)}
             <div class="profile-grid my-1">
                 <ProfileTop profile={profile} />
+                {<ProfileAbout profile={profile} />}
             </div>
         </Fragment>}
     </Fragment>
