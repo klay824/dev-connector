a1e524ec82ba1f831f28baed298113fb20c00dda
Katy Chadwell <kchadwell0226@gmail.com>
2021-09-28 22:00:43 -0500

    single post display

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
 /* eslint-disable default-case */
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST, ADD_POST } from '../actions/types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST, ADD_POST, GET_POST } from '../actions/types';
 
 const initialState = {
     posts: [],
@@ -17,6 +17,12 @@ export default function (state = initialState, action) {
             return {
                 ...state,
                 posts: payload,
+                loading: false
+            };
+        case GET_POST:
+            return {
+                ...state,
+                post: payload,
                 loading: false
             };
         case ADD_POST:
